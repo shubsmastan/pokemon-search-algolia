865a24945c5ebbf51cad2f188d37969145d7c199
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2024-04-30 18:50:04 +0100

    Styling and icons to search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import pokemonLogo from '/pokemon-logo.svg';
+
 export const SearchBar = () => {
 	return (
 		<div className='bg-slate-300 dark:bg-slate-700 p-5 rounded-xl flex gap-5 h-20'>
-			<p className='w-32'>Search Bar</p>
-			<input type='text' placeholder='search' className='w-full'></input>
+			<div className='flex flex-col justify-center w-44'>
+				<img src={pokemonLogo} style={{ height: 40 }} />
+				<p className='text-xs text-center'>
+					Powered by{' '}
+					<a
+						href='https://www.algolia.com/'
+						className='text-blue-700 dark:text-blue-300'>
+						Algolia
+					</a>
+				</p>
+			</div>
+			<div className='bg-white w-full flex p-3 gap-3 rounded-md focus-within:outline focus-within:outline-offset-4  focus-within:outline-blue-700 dark:focus-within:outline-blue-300'>
+				<FontAwesomeIcon
+					icon={faMagnifyingGlass}
+					style={{ color: '#0f172a' }}
+				/>
+				<input
+					type='text'
+					placeholder='Search'
+					className='w-full focus:outline-none text-slate-900'></input>
+			</div>
 		</div>
 	);
 };
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,7 +1,7 @@
 export const SearchResults = () => {
 	return (
 		<div className='bg-slate-300 dark:bg-slate-700 p-5 rounded-xl flex gap-5 h-full w-5/6'>
-			<p>Search Results</p>
+			<p>Type a search term to see some Pokémon!</p>
 		</div>
 	);
 };
