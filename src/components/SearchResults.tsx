18db78379eda7b8968b8908b62afb7b28f8cae01
Shoaib Mastan <114753586+shubsmastan@users.noreply.github.com>
2024-05-01 12:40:30 +0100

    Move pagination to bottom

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -18,18 +18,18 @@ export const SearchResults = () => {
 			<h2 className='absolute text-sm top-3 left-5 lg:left-auto lg:right-8'>
 				{hits.length} results
 			</h2>
+			<Hits
+				hitComponent={PokemonCard}
+				classNames={{
+					list: 'grid grid-cols-1 gap-x-5 gap-y-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5',
+				}}
+			/>
 			<Pagination
 				classNames={{
 					list: 'flex gap-3 justify-center items-center text-3xl text-blue-600 dark:text-blue-400 sm:gap-5',
 					pageItem: 'text-lg mt-2',
 					selectedItem: 'text-black dark:text-white',
 					disabledItem: 'text-black dark:text-white',
-				}}
-			/>
-			<Hits
-				hitComponent={PokemonCard}
-				classNames={{
-					list: 'grid grid-cols-1 gap-x-5 gap-y-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5',
 				}}
 			/>
 		</div>
